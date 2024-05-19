3252584fdb930eab6fb453c3f88bc8e185295089
Volodymyr Bohdan <volodymyr.bohdan.pe@gmail.com>
2024-05-19 10:21:38 +0300

    Implement admin user initialization

--- backend/db/session.py
+++ backend/db/session.py
@@ -4,10 +4,15 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from core import config
 from db.models import Base
+import contextlib
 
 engine = create_async_engine(config.SQLALCHEMY_DATABASE_URI, future=True, echo=True)
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
 
 
 async def get_db() -> AsyncGenerator:
@@ -16,6 +21,18 @@ async def get_db() -> AsyncGenerator:
     """Dependency for getting session"""
     try:
         session: AsyncSession = async_session()
+        yield session
+    finally:
+        await session.close()
+
+
+# only for admin user init
+@contextlib.asynccontextmanager
+async def get_db2():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    session = AsyncSessionLocal()
+    try:
         yield session
     finally:
         await session.close()
--- backend/tasks.py
+++ backend/tasks.py
@@ -1,10 +1,18 @@
 import logging
 
+from fastapi import Depends
+
 from celery_app import celery_app
 from celery.exceptions import SoftTimeLimitExceeded
 from celery.states import SUCCESS, STARTED
 from celery.result import AsyncResult
 from core import config
+from core.hashing import Hasher
+from db.dals import UserDAL
+from db.session import get_db2
+from sqlalchemy.ext.asyncio import AsyncSession
+from celery.signals import worker_ready
+import asyncio
 
 
 logger = logging.getLogger(__name__)
@@ -17,3 +25,37 @@ logger = logging.getLogger(__name__)
 def add_book(self, request_data: str):
     logger.info("Start main background celery task book")
     return True
+
+
+@worker_ready.connect
+def at_start(sender, **kwargs):
+    async def init_admin_user():
+        logger.info("Start celery task to init admin user")
+        try:
+            async with get_db2() as session:
+                user_dal = UserDAL(session)
+                user_created = await user_dal.get_user_by_email(
+                    email=config.ADMIN_EMAIL
+                )
+                if not user_created:
+                    user = await user_dal.create_user(
+                        email=config.ADMIN_EMAIL,
+                        first_name=config.ADMIN_FIRST_NAME,
+                        last_name=config.ADMIN_LAST_NAME,
+                        hashed_password=Hasher.get_password_hash(config.ADMIN_PASSWORD),
+                        is_superuser=True,
+                    )
+                    logger.info(f"Created user: {user.email}")
+
+                    await session.commit()  # Ensure the transaction is committed
+
+                    if user is not None:
+                        return True
+                else:
+                    logger.info("Admin user %s already exists", config.ADMIN_EMAIL)
+        except Exception as e:
+            logger.error(f"Failed to create admin user: {e}")
+            await session.rollback()  # Explicitly roll back in case of error
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_admin_user())
